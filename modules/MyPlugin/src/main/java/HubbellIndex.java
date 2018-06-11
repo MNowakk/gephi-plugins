178b9c296b650d4d35628c420270f4855e768fc4
MNowakk <monika.1995@poczta.fm>
2018-06-11 21:39:42 +0200

    Ver. 1

---
+++ modules/MyPlugin/src/main/java/HubbellIndex.java
@@ -0,0 +1,10 @@
+public class HubbellIndex {
+
+    public static void main(String[] args) {
+        HubbellIndex index = new HubbellIndex();
+        index.init();
+    }
+
+    private void init() {
+    }
+}
